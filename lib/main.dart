bf1f4dc372c9efad3d7c7cb128e4a201e613cf02
yuri tamura <yuri.tam8@gmail.com>
2022-06-27 20:55:42 +0900

    完了へのボタンと完了タスクリストの作成。

--- lib/main.dart
+++ lib/main.dart
@@ -25,6 +25,11 @@ class Home extends StatefulWidget {
 
 class _TodoListPageState extends State<Home> {
   List<Map<String, dynamic>> items = [];
+  List<Map<String, dynamic>> complete = [];
+
+  void _removeItems(items, index) {
+    items.removeAt(index);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -52,33 +57,105 @@ class _TodoListPageState extends State<Home> {
           child: Icon(Icons.add),
         ),
         body: Container(
-              child: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: ListView.builder(
-                  itemCount: items.length,
-                  itemBuilder: (context, index) {
-                    return Card(
-                      child: Container(
-                        width: double.infinity,
-                        padding: const EdgeInsets.all(10.0),
-                        child: Column(
-                          children: [
-                            Text(items[index]["date"]),
-                            Text(items[index]["text"]),
-                            ElevatedButton(
-                                onPressed: () {
-                                  setState(() {
-                                    items.removeAt(index);
-                                    print(items);
-                                  });
-                            },
-                                child: Text("削除"))
-                          ],
-                        ),
-                      ),
-                    );
-                  },
-                ),
+              child: Column(
+                children: <Widget>[
+                  Container(
+                      padding: EdgeInsets.all(20.0),
+                      child: Text(
+                        "未完了タスク",
+                        style: TextStyle(fontWeight: FontWeight.bold, fontSize: 22.0),
+                      )
+                  ),
+                  Expanded(
+                    child: ListView.builder(
+                      itemCount: items.length,
+                      itemBuilder: (context, index) {
+                        return Card(
+                          child: Container(
+                            width: double.infinity,
+                            padding: const EdgeInsets.all(10.0),
+                            child: Column(
+                              children: <Widget>[
+                                Text(items[index]["date"]),
+                                Text(items[index]["text"]),
+                                Row(
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                  children: <Widget>[
+                                    ElevatedButton(
+                                        onPressed: () {
+                                          setState(() {
+                                            complete.add(items[index]);
+                                            _removeItems(items, index);
+
+                                          });
+                                    },
+                                        child: Text("完了へ")),
+                                    SizedBox(width: 20),
+                                    ElevatedButton(
+                                        onPressed: () {
+                                          setState(() {
+                                            _removeItems(items, index);
+                                          });
+                                        },
+                                        child: Text("削除")),
+                                  ],
+                                ),
+                              ],
+                            ),
+                          ),
+                        );
+                      },
+                    ),
+                  ),
+                  Container(
+                      padding: EdgeInsets.all(20.0),
+                      child: Text(
+                        "完了タスク",
+                        style: TextStyle(fontWeight: FontWeight.bold, fontSize: 22.0),
+                      )
+                  ),
+                  Expanded(
+                    child: ListView.builder(
+                      itemCount: complete.length,
+                      itemBuilder: (context, index) {
+                        return Card(
+                          child: Container(
+                            width: double.infinity,
+                            padding: const EdgeInsets.all(10.0),
+                            child: Column(
+                              children: <Widget>[
+                                Text(complete[index]["date"]),
+                                Text(complete[index]["text"]),
+                                Row(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: <Widget>[
+                                    ElevatedButton(
+                                        onPressed: () {
+                                          setState(() {
+                                            items.add(complete[index]);
+                                            _removeItems(complete, index);
+
+                                          });
+                                        },
+                                        child: Text("未完了へ")),
+                                    SizedBox(width: 20),
+                                    ElevatedButton(
+                                        onPressed: () {
+                                          setState(() {
+                                            _removeItems(complete, index);
+                                          });
+                                        },
+                                        child: Text("削除")),
+                                  ],
+                                ),
+                              ],
+                            ),
+                          ),
+                        );
+                      },
+                    ),
+                  ),
+                ],
               ),
             ),
     );
